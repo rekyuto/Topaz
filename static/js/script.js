a3cf1535c83d85c121ad0ee94e4d8d03f42c2a74
rekyuto <botejacob3@gmail.com>
2023-11-14 01:07:36 -0800

    added audio + plays when clicked, README update, added link to personal website

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,21 @@
 let score = 0;
+
 function scoreCounter() {
-    score = score + 1
+    score = score + 1;
 	let scoreboard = document.getElementById('score');
 	scoreboard.innerHTML = score;
+	playAudio();
+}
+
+var topazAudio = [
+	new Audio("static/audio/topaz1.wav"),
+	new Audio("static/audio/topaz2.wav"),
+	new Audio("static/audio/topaz3.wav"),
+	new Audio("static/audio/topaz4.wav"),
+];
+function playAudio() {
+	var audio;
+	var random = Math.floor(Math.random() * 4);
+	audio = topazAudio[random].cloneNode();
+	audio.play();
 }
